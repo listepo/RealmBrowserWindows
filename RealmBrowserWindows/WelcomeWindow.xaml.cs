aa301b487978ee84d556f82ad8665590ec361828
Ivan Tugay <listepo@gmail.com>
2017-07-31 19:36:57 +0300

    Added Welcome window

---
+++ RealmBrowserWindows/WelcomeWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Reflection;
+using Microsoft.Win32;
+using Realms;
+
+namespace RealmBrowserWindows
+{
+    /// <summary>
+    /// Interaction logic for WelcomeWindow.xaml
+    /// </summary>
+    public partial class WelcomeWindow : Window
+    {
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+            AssemblyVersion.Content = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void OpenRealmFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            bool? userClickedOK = openFileDialog.ShowDialog();
+            if (userClickedOK == true) {
+                var config = new RealmConfiguration(openFileDialog.FileName);
+                var realm = Realm.GetInstance(config);
+            }
+        }
+    }
+}
